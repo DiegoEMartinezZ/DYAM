d06c22e4d4f51c8b992a29da4d8a5413c8dc3345
DiegoEMartinezZ <diermaza.23@gmail.com>
2024-02-24 00:28:19 -0500

    fix the menu window when app open in a smartphone, need to improve the favicon and the background images

--- src/assets/menu/Menu.jsx
+++ src/assets/menu/Menu.jsx
@@ -1,24 +1,8 @@
 import "./menu.css";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSortDown } from "@fortawesome/free-solid-svg-icons";
 import { HashLink as Link } from "react-router-hash-link";
 
 const Menu = ({ menuHandle }) => {
-  const planExtendedMenu = () => {
-    const planExtended = document.querySelector(".plan-list");
-    const planButton = document.querySelector("button.plan-button");
-    planExtended.classList.toggle("active");
-    planButton.classList.toggle("active-button");
-  };
-
-  const priceExtendedMenu = () => {
-    const priceExtended = document.querySelector(".price-list");
-    const priceButton = document.querySelector("button.price-button");
-    priceExtended.classList.toggle("active");
-    priceButton.classList.toggle("active-button");
-  };
-
   return (
     <>
       <section className="menu-container">
@@ -26,51 +10,15 @@ const Menu = ({ menuHandle }) => {
           <Link to="#about" className="options" onClick={menuHandle}>
             <li> About us </li>
           </Link>
-
           <Link to="#team" className="options" onClick={menuHandle}>
             <li>Team</li>
           </Link>
-          <li>
-            <div>
-              Plans
-              <button onClick={planExtendedMenu} className="plan-button">
-                <FontAwesomeIcon icon={faSortDown} />
-              </button>
-            </div>
-            <ul className="plan-list">
-              <Link to="#approach" className="options" onClick={menuHandle}>
-                <li> Strategic Approach </li>
-              </Link>
-
-              <Link to="#campaign" className="options" onClick={menuHandle}>
-                <li> Campaign Review </li>
-              </Link>
-
-              <Link to="#reporting" className="options" onClick={menuHandle}>
-                <li> Campaing Reporting </li>
-              </Link>
-
-              <Link to="#design" className="options" onClick={menuHandle}>
-                <li> Design </li>
-              </Link>
-            </ul>
-          </li>
-          <li>
-            <div>
-              Prices
-              <button onClick={priceExtendedMenu} className="price-button">
-                <FontAwesomeIcon icon={faSortDown} />
-              </button>
-            </div>
-            <ul className="price-list">
-              <Link to="#consulting" className="options" onClick={menuHandle}>
-                <li> Consulting Fee </li>
-              </Link>
-              <Link to="#services" className="options" onClick={menuHandle}>
-                <li> AdOps, Creative, Website Service Fee </li>
-              </Link>
-            </ul>
-          </li>
+          <Link to="#approach" className="options" onClick={menuHandle}>
+            <li> Plans</li>
+          </Link>
+          <Link to="#consulting" className="options" onClick={menuHandle}>
+            <li> Price </li>
+          </Link>
         </ul>
       </section>
     </>
